b070ddee5e0b7af34299b428fbe0c469738ef03d
feteu <29181278+feteu@users.noreply.github.com>
2024-12-27 19:54:06 +0100

    add module docstring to describe ASGI Request Duration package functionality

--- asgi_request_duration/__init__.py
+++ asgi_request_duration/__init__.py
@@ -1,3 +1,9 @@
+"""
+ASGI Request Duration Package
+
+This package provides middleware and utilities to measure and log the duration of HTTP requests in ASGI applications.
+"""
+
 from asgi_request_duration.context import REQUEST_DURATION_CTX_KEY, request_duration_ctx_var
 from asgi_request_duration.exceptions import InvalidHeaderNameException, PrecisionValueOutOfRangeException
 from asgi_request_duration.filters import RequestDurationFilter
