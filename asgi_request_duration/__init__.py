fbd6f6522fa7a3e7b91ec715d92007c2f0054392
feteu <29181278+feteu@users.noreply.github.com>
2024-12-27 19:52:44 +0100

    refactor __all__ declaration to use tuple syntax

--- asgi_request_duration/__init__.py
+++ asgi_request_duration/__init__.py
@@ -3,11 +3,11 @@ from asgi_request_duration.exceptions import InvalidHeaderNameException, Precisi
 from asgi_request_duration.filters import RequestDurationFilter
 from asgi_request_duration.middleware import RequestDurationMiddleware
 
-__all__ = [
+__all__ = (
     "InvalidHeaderNameException",
     "PrecisionValueOutOfRangeException",
     "REQUEST_DURATION_CTX_KEY",
     "request_duration_ctx_var",
     "RequestDurationFilter",
     "RequestDurationMiddleware",
-]
+)
